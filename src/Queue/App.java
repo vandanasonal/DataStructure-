aa4ac394aebff1c597aa332d52a17df2718a8b6c
vandanasonal <vandanasonal12391@gmail.com>
2017-11-07 21:47:36 +0530

    Create App.java

---
+++ src/Queue/App.java
@@ -0,0 +1,18 @@
+package Queue;
+
+public class App {
+	public static void main(String[] args) {
+		Queue <Integer> myQueue= new Queue<Integer>();
+		
+		myQueue.enqueue(10);
+		myQueue.enqueue(100);
+		myQueue.enqueue(1000);
+		
+		System.out.println(myQueue.size());
+		
+		System.out.println(myQueue.dequeue());
+		System.out.println(myQueue.dequeue());
+		System.out.println(myQueue.dequeue());
+	}
+
+}
